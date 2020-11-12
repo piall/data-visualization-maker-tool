e8b493ee3c3dbdb2d732c9ce09f66a01506497a2
mdPial <root.pial@gmail.com>
2020-11-12 19:11:23 +0600

    added button in the component

--- components/Card.vue
+++ components/Card.vue
@@ -1,14 +1,17 @@
 <template>
-  <b-card
-    :img-src="visualizationInfo.imageAddress"
-    img-alt
-    img-top
-    tag="article"
-    style="max-width: 25rem;"
-    class="mb-2"
-  >
-    <b-card-text>{{ visualizationInfo.visualizationName }}</b-card-text>
-  </b-card>
+  <center>
+    <b-card
+      :img-src="visualizationInfo.imageAddress"
+      img-alt
+      img-top
+      tag="article"
+      style="max-width: 25rem;"
+      class="mb-2 my-3"
+    >
+      <b-card-text>{{ visualizationInfo.visualizationName }}</b-card-text>
+      <b-button :href="visualizationInfo.routingAddress" variant="primary">বিস্তারিত</b-button>
+    </b-card>
+  </center>
 </template>
 
 <script>
@@ -22,18 +25,20 @@ export default {
 </script>
 
 <style scoped>
-.card {
+/* .card {
   margin: 0 auto;
   float: none;
   margin: 15px;
   padding: 0px 0px;
   align-items: center;
-}
+} */
+
+
 
 .card:hover {
   cursor: pointer;
 }
-.card-text {
+.card-text, .btn-primary {
   font-family: "Baloo Da 2";
   font-weight: 600;
 }
