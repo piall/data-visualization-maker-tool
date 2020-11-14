97d0608b01bfafb74820e696c8b47bcc7121c5be
mdPial <root.pial@gmail.com>
2020-11-14 12:04:06 +0600

    added logic to toggle

--- pages/visualization/pieChart.vue
+++ pages/visualization/pieChart.vue
@@ -1,5 +1,26 @@
 <template>
   <div>
-    <FileOption />
+    <FileOption @manualInput="change" v-if="!manualDataInput"/>
+    <section v-else>
+      
+    </section>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      manualDataInput: false
+    };
+  },
+  methods: {
+    change() {
+      this.manualDataInput = true;
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped>
+</style>
