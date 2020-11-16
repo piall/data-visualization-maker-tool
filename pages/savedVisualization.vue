76ee88d191ddc7dba266063e60f9202276960e27
mdPial <root.pial@gmail.com>
2020-11-16 18:13:06 +0600

    initial commit

---
+++ pages/savedVisualization.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <center class="my-3">
+      <h3 v-if="!savedVisualization">আপনার সেভ করা কোন ভিজুয়ালাইজেশন নেই</h3>
+    </center>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      savedVisualization: null
+    };
+  }
+};
+</script>
+
+<style scoped>
+* {
+  font-family: "Baloo Da 2";
+  font-weight: 600;
+}
+</style>
