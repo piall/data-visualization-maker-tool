b259e2d931f08177e8ae12d9f1fb7aef369c6db4
mdPial <root.pial@gmail.com>
2020-11-27 19:00:27 +0600

    updated layout

--- pages/visualizations.vue
+++ pages/visualizations.vue
@@ -2,18 +2,43 @@
   <div>
     <b-container fluid>
       <b-row class="p-5">
-        <b-col
-          sm="12"
-          md="6"
-          lg="4"
-          xl="3"
-          class="cards"
-          v-for="index in names"
-          :key="index.visualizationName"
-        >
-          <Card :visualizationInfo="index" />
+        <b-col md="12" lg="4">
+          <ul>
+            <li :class="{active:manualInput}" @click="toggleManualInput">
+              <i class="fa fa-clipboard"></i>
+              ম্যানুয়াল ইনপুট
+            </li>
+            <li :class="{active:fileUpload}" @click="toggleFileUpload">
+              <i class="fa fa-upload"></i>
+              আপলোড করুন
+            </li>
+          </ul>
+        </b-col>
+        <b-col>
+          <div v-if="manualInput">
+            <b-form-textarea id="textarea-rows" placeholder="আপনার JSON ডেটা পেস্ট করুন" rows="8"></b-form-textarea>
+          </div>
+          <div v-if="fileUpload" class="box">
+            <p>ফাইল আপলোড করুন</p>
+            <input
+              type="file"
+              style="background-color:#2f2e41; color: white; padding:3px; border-radius:4px"
+            />
+          </div>
         </b-col>
       </b-row>
+      <p>ভিজুয়ালেইজেশন এর ধরন বাছুন</p>
+      <b-row>
+        <b-col>Hexagonal Binning</b-col>
+        <b-col>Contour Plot</b-col>
+        <b-col>Circular Dendrogram</b-col>
+        <b-col>Cluster Dendrogram</b-col>
+        <b-col>Multi-Line Chart</b-col>
+        <b-col>Pie chart</b-col>
+        <b-col>Multi-Line Chart</b-col>
+        <b-col>Multi-Line Chart</b-col>
+      </b-row>
+      <p>কোন অক্ষে কোন কলাম থাকবে</p>
     </b-container>
   </div>
 </template>
@@ -22,46 +47,45 @@
 export default {
   data() {
     return {
-      names: [
-        {
-          visualizationName: "হিস্টোগ্রাম (Histogram)",
-          imageAddress: "/_nuxt/assets/login.svg",
-          routingAddress: "./visualization/histogram"
-        },
-        {
-          visualizationName: "পাই চার্ট (Pie Chart)",
-          imageAddress: "/_nuxt/assets/login.svg",
-          routingAddress: "./visualization/pieChart"
-        },
-        {
-          visualizationName: "বার চার্ট (Bar Chart)",
-          imageAddress: "/_nuxt/assets/login.svg",
-          routingAddress: "./visualization/barChart"
-        },
-        {
-          visualizationName: "স্কেটার প্লট (Scatter Plot)",
-          imageAddress: "/_nuxt/assets/login.svg",
-          routingAddress: "./visualization/scatterPlot"
-        },
-        {
-          visualizationName: "লাইন চার্ট (Line Chart)",
-          imageAddress: "/_nuxt/assets/login.svg",
-          routingAddress: "./visualization/lineChart"
-        },
-        {
-          visualizationName: "হিট ম্যাপ (Heat Map)",
-          imageAddress: "/_nuxt/assets/login.svg",
-          routingAddress: "./visualization/heatMap"
-        }
-      ]
-      // screenWidth: this.$screen.width + "px",
-      // screenHeight: this.$screen.height + "px",
+      manualInput: true,
+      fileUpload: false
     };
   },
-  methods: {},
+  methods: {
+    toggleFileUpload() {
+      this.manualInput = false;
+      this.fileUpload = true;
+    },
+    toggleManualInput() {
+      this.manualInput = true;
+      this.fileUpload = false;
+    },
+    selectFile() {}
+  },
   watch: {}
 };
 </script>
 
 <style scoped>
+li {
+  margin: 2px;
+  padding: 4px;
+  list-style-type: none;
+}
+li:hover {
+  cursor: pointer;
+}
+.active {
+  background-color: #2f2e41;
+  color: white;
+  border-radius: 4px;
+}
+.box {
+  border-style: dotted;
+  text-align: center;
+  padding: 90px 0;
+  color: #2f2e41;
+  font-weight: bold;
+  cursor: pointer;
+}
 </style>
