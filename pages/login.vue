d8c6e1a7b581d7957777a5f1cc9a989f282e1aef
mdPial <root.pial@gmail.com>
2020-11-27 18:59:25 +0600

    updated

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
-    <Login />
+    <b-row>
+      <b-col xs="8" md="8" lg="6" xl="4" offset-md="2" offset-lg="3" offset-xl="4">
+        <Login />
+      </b-col>
+    </b-row>
   </div>
 </template>
