d1767425faed363b62db9d9f67ce0ae2d401a99e
mdPial <root.pial@gmail.com>
2020-11-27 18:58:39 +0600

    updated

--- components/Register.vue
+++ components/Register.vue
@@ -1,71 +1,72 @@
 <template>
-  <center>
-    <b-container fluid="lg">
-      <b-card
-        title="রেজিস্ট্রেশন করুন"
-        img-src=".././assets/man_standing.svg"
-        img-alt="Image"
-        img-top
-        tag="article"
-        style="margin-top:40px;"
-        class="mb-2"
-      >
-        <b-container fluid>
-          <b-row class="my-2">
-            <!-- <b-col sm="3">
+  <b-container fluid="lg">
+    <b-card
+      title="রেজিস্ট্রেশন করুন"
+      img-src=".././assets/man_standing.svg"
+      img-alt="Image"
+      img-top
+      tag="article"
+      style="margin-top:40px"
+      class="mb-2"
+    >
+      <b-container fluid>
+        <b-row class="my-2">
+          <!-- <b-col sm="3">
       <label for="input-none">ইমেইল:</label>
-            </b-col>-->
-            <b-col sm="12">
-              <b-form-input
-                id="input-none"
-                type="email"
-                :state="null"
-                placeholder="আপনার ইমেইল দিন"
-              ></b-form-input>
-              <!-- state null will make like normal input box -->
-            </b-col>
-          </b-row>
+          </b-col>-->
+          <b-col>
+            <b-form-input id="input-none" type="email" :state="null" placeholder="আপনার ইমেইল দিন"></b-form-input>
+            <!-- state null will make like normal input box -->
+          </b-col>
+        </b-row>
 
-          <b-row class="my-2">
-            <!-- <b-col sm="3">
+        <b-row class="my-2">
+          <!-- <b-col sm="3">
       <label for="input-valid">পাসওয়ার্ড:</label>
-            </b-col>-->
-            <b-col sm="12">
-              <b-form-input
-                id="input-valid"
-                type="password"
-                :state="null"
-                placeholder="আপনার পাসওয়ার্ড দিন"
-              ></b-form-input>
-              <!-- state true will make the icon green -->
-            </b-col>
-          </b-row>
-          <b-row class="my-2">
-            <b-col sm="12">
-              <b-form-input
-                id="input-valid"
-                type="password"
-                :state="null"
-                placeholder="পুনরায় আপনার পাসওয়ার্ড দিন"
-              ></b-form-input>
-              <!-- state false will make the icon cross -->
-            </b-col>
-          </b-row>
-        </b-container>
-        <b-button variant="primary">একাউন্ট খুলুন</b-button>
-        <hr />
-        <b-button variant="success" href="./login">লগইন করুন</b-button>
-      </b-card>
-    </b-container>
-  </center>
+          </b-col>-->
+          <b-col>
+            <b-form-input
+              id="input-valid"
+              type="password"
+              :state="null"
+              placeholder="আপনার পাসওয়ার্ড দিন"
+            ></b-form-input>
+            <!-- state true will make the icon green -->
+          </b-col>
+        </b-row>
+        <b-row class="my-2">
+          <b-col>
+            <b-form-input
+              id="input-valid"
+              type="password"
+              :state="null"
+              placeholder="পুনরায় আপনার পাসওয়ার্ড দিন"
+            ></b-form-input>
+            <!-- state false will make the icon cross -->
+          </b-col>
+        </b-row>
+      </b-container>
+      <b-button variant="primary">একাউন্ট খুলুন</b-button>
+      <hr />
+      <b-button variant="success" href="./login">লগইন করুন</b-button>
+    </b-card>
+  </b-container>
 </template>
 
 <style lang="css" scoped>
+div.container-lg {
+  /* background-color: aqua; */
+  /* width: 100%; */
+  display: flex;
+  align-items: center;
+  text-align: center;
+}
 .card {
-  margin: 155px;
+  /* margin: 155px; */
   padding: 20px;
   font-family: "Baloo Da 2";
-  max-width: 50%;
+
+  /* max-width: 50%; */
 }
 
 .card-img-top {
