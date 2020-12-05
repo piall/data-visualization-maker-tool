19bea8aaef30b508c95b223a911e2e23fa7b66f1
mdPial <root.pial@gmail.com>
2020-12-05 11:34:43 +0600

    updated

--- pages/visualizations.vue
+++ pages/visualizations.vue
@@ -4,11 +4,11 @@
       <b-row class="p-5">
         <b-col md="12" lg="4">
           <ul>
-            <li :class="{active:manualInput}" @click="toggleManualInput">
+            <li :class="{ active: manualInput }" @click="toggleManualInput">
               <i class="fa fa-clipboard"></i>
               ম্যানুয়াল ইনপুট
             </li>
-            <li :class="{active:fileUpload}" @click="toggleFileUpload">
+            <li :class="{ active: fileUpload }" @click="toggleFileUpload">
               <i class="fa fa-upload"></i>
               আপলোড করুন
             </li>
@@ -16,7 +16,12 @@
         </b-col>
         <b-col>
           <div v-if="manualInput">
-            <b-form-textarea id="textarea-rows" placeholder="আপনার JSON ডেটা পেস্ট করুন" rows="8"></b-form-textarea>
+            <b-form-textarea
+              id="textarea-rows"
+              placeholder="আপনার JSON ডেটা পেস্ট করুন"
+              rows="8"
+              v-model="inputData"
+            ></b-form-textarea>
           </div>
           <div v-if="fileUpload" class="box">
             <p>ফাইল আপলোড করুন</p>
@@ -27,28 +32,94 @@
           </div>
         </b-col>
       </b-row>
-      <p>ভিজুয়ালেইজেশন এর ধরন বাছুন</p>
+      <!-- <p>ভিজুয়ালেইজেশন এর ধরন বাছুন</p>
+
       <b-row>
-        <b-col>Hexagonal Binning</b-col>
-        <b-col>Contour Plot</b-col>
-        <b-col>Circular Dendrogram</b-col>
-        <b-col>Cluster Dendrogram</b-col>
-        <b-col>Multi-Line Chart</b-col>
-        <b-col>Pie chart</b-col>
-        <b-col>Multi-Line Chart</b-col>
-        <b-col>Multi-Line Chart</b-col>
+        <b-col md="4">
+          <Card />
+        </b-col>
       </b-row>
       <p>কোন অক্ষে কোন কলাম থাকবে</p>
+      <b-row>
+        x-axis
+        <b-col>
+          <input type="checkbox" @change="check()" />
+          column1
+          <br />
+          <input type="checkbox" @change="check()" />
+          column-2
+          <br />
+        </b-col>
+      </b-row>
+      <b-row>
+        y-axis
+        <b-col>
+          <input type="checkbox" @change="check()" />
+          column1
+          <br />
+          <input type="checkbox" @change="check()" />
+          column-2
+          <br />
+        </b-col>
+      </b-row> -->
+      {{ inputData }}
     </b-container>
   </div>
 </template>
-
+<script src="https://d3js.org/d3.v6.min.js"></script>
 <script>
+// <b-col>Hexagonal Binning</b-col>
+// <b-col>Contour Plot</b-col>
+// <b-col>Circular Dendrogram</b-col>
+// <b-col>Cluster Dendrogram</b-col>
+// <b-col>Multi-Line Chart</b-col>
+// <b-col>Pie chart</b-col>
+// <b-col>Multi-Line Chart</b-col>
+// <b-col>Multi-Line Chart</b-col>
+// import * as d3 from "d3";
+
 export default {
   data() {
     return {
+      a: [1, 2, 3],
+      b: ["a", "b", "c"],
+      row: "",
+      col: "",
+      inputData: "",
       manualInput: true,
-      fileUpload: false
+      fileUpload: false,
+      names: [
+        {
+          visualizationName: "হিস্টোগ্রাম (Histogram)",
+          imageAddress: "/_nuxt/assets/histogram.svg",
+          routingAddress: "./visualization/histogram"
+        },
+        {
+          visualizationName: "পাই চার্ট (Pie Chart)",
+          imageAddress: "/_nuxt/assets/pie_chart.png",
+          routingAddress: "./visualization/pieChart"
+        },
+        {
+          visualizationName: "বার চার্ট (Bar Chart)",
+          imageAddress: "/_nuxt/assets/bar_chart.png",
+          routingAddress: "./visualization/barChart"
+        },
+        {
+          visualizationName: "স্কেটার প্লট (Scatter Plot)",
+          imageAddress: "/_nuxt/assets/scatter_plot.png",
+          routingAddress: "./visualization/scatterPlot"
+        },
+        {
+          visualizationName: "লাইন চার্ট (Line Chart)",
+          imageAddress: "/_nuxt/assets/login.svg",
+          routingAddress: "./visualization/lineChart"
+        },
+        {
+          visualizationName: "হিট ম্যাপ (Heat Map)",
+          imageAddress: "/_nuxt/assets/login.svg",
+          routingAddress: "./visualization/heatMap"
+        }
+      ]
     };
   },
   methods: {
@@ -60,9 +131,19 @@ export default {
       this.manualInput = true;
       this.fileUpload = false;
     },
-    selectFile() {}
+    selectFile() {},
+    x_check() {
+      this.row = this.a;
+    },
+    y_check() {
+      this.col = this.b;
+    }
   },
-  watch: {}
+  watch: {
+    inputData: function(val) {
+      this.inputData = JSON.parse(val);
+    }
+  }
 };
 </script>
 
