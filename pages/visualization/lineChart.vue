1dca84ec5e73a8d92049d3f9160fd9dfc8d58e59
mdPial <root.pial@gmail.com>
2020-12-05 11:32:58 +0600

    initial commit

---
+++ pages/visualization/lineChart.vue
@@ -0,0 +1,158 @@
+<template>
+<div>
+    <center>
+      <h5 class="my-2">ব্যাবহারবিধি</h5>
+      <ul>
+        <li>নতুন সারি বানানোর জন্য enter চাপুন</li>
+        <li>পরের সারিতে যাওয়ার জন্য tab চাপুন</li>
+        <li>সারি বাদ দেয়ার জন্য Ctrl + click করুন</li>
+      </ul>
+      <b-card-group deck class="mx-1 my-3">
+        <b-card header="আপনার ডেটা দিন" header-tag="header">
+          <b-row
+            class="my-2"
+            v-for="(dataObjects, ArrayIndex) in chart_data"
+            :key="ArrayIndex"
+          >
+            <b-col
+              sm="12"
+              md="4"
+              v-for="(data, ObjectKey) in dataObjects"
+              :key="ObjectKey"
+              class="my-1"
+            >
+              <b-form-input
+                type="number"
+                :state="null"
+                placeholder="ভ্যলু-১"
+                @keyup.enter="addRowToColumn"
+                @click.ctrl="deleteRowOfColumn(ArrayIndex)"
+                v-model="chart_data[ArrayIndex][ObjectKey]"
+                v-if="ObjectKey == 'value1'"
+              ></b-form-input>
+              <b-form-input
+                type="number"
+                :state="null"
+                placeholder="ভ্যালু-২"
+                @keyup.enter="addRowToColumn"
+                @click.ctrl="deleteRowOfColumn(ArrayIndex)"
+                v-model="chart_data[ArrayIndex][ObjectKey]"
+                v-if="ObjectKey == 'value2'"
+              ></b-form-input>
+              <b-form-input
+                type="number"
+                :state="null"
+                placeholder="ভ্যালু-৩"
+                @keyup.enter="addRowToColumn"
+                @click.ctrl="deleteRowOfColumn(ArrayIndex)"
+                v-model="chart_data[ArrayIndex][ObjectKey]"
+                v-if="ObjectKey == 'value3'"
+              ></b-form-input>
+            </b-col>
+          </b-row>
+        </b-card>
+      </b-card-group> 
+      <D3LineChart 
+        :config="chart_config"
+        :datum="chart_data"
+        v-if="reload && chart_data[0].value1!='' && chart_data[0].value2!='' && chart_data[0].value3!=''"
+      ></D3LineChart >
+    </center>
+  </div>
+</template>
+
+<script>
+import { D3LineChart  } from 'vue-d3-charts';
+
+export default {
+  components: {
+    D3LineChart ,
+  },
+  data() {
+    return {
+      reload: true,
+      chart_data: [
+        {value1: "", value2: "", value3: ""},
+      ],
+      chart_config: {
+        values: ['value1', 'value2'],
+        date: {
+          key: 'value3',
+          inputFormat: '%Y',
+          outputFormat: '%Y',
+        },
+        points:false,
+        axis: {
+          yTicks: 3,
+        }
+      },
+      count:2010
+    };
+  },
+   methods: {
+    change() {
+      this.manualDataInput = true;
+    },
+    addRowToColumn() {
+      this.chart_data.push({ value1: "", value2: "", value3: "" });
+    },
+    deleteRowOfColumn(index) {
+      if (this.chart_data.length > 1) {
+        this.chart_data.splice(index, 1);
+      }
+      console.log(index);
+    }
+  },
+  watch: {
+    chart_data: {
+      handler: function() {
+        console.log("changed data");
+        this.reload = false;
+        this.$nextTick(function() {
+          this.reload = true;
+        });
+      },
+      deep: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type="number"] {
+  -moz-appearance: textfield;
+}
+* {
+  font-family: "Baloo Da 2";
+}
+
+h6 {
+  font-weight: bold;
+  font-size: 17px;
+}
+
+h5 {
+  font-family: "Baloo Da";
+}
+
+li {
+  font-family: "Baloo Da 2";
+  font-size: 18px;
+  font-weight: 500;
+}
+
+.card{
+    background-color: #faf7c2;
+    /* color:white; */
+    font-weight: bold;
+    border: none;
+}
+</style>
