872b4bc17f48e2db7ad25542105cb1496bbcf13f
mdPial <root.pial@gmail.com>
2020-12-05 19:33:54 +0600

    updated

--- pages/visualization/barChart/index.vue
+++ pages/visualization/barChart/index.vue
@@ -3,14 +3,14 @@
     <!-- <b-button href="./barChart/fiveValue" variant="warning">ভ্যালু - ৫</b-button> -->
     <div class="mb-2 my-3">
           <div class="row no-gutters d-flex justify-content-center">
-            <div class="cols">
+            <!-- <div class="cols">
               <b-avatar  href="/visualization/barChart" class="mx-4" src="~assets/D3BarChart.svg" size="8rem"></b-avatar>
               <p>বার চার্ট</p>
             </div>
             <div class="cols">
               <b-avatar  href="/visualization/barChart" class="mx-4" src="~assets/D3BarChart.svg" size="8rem"></b-avatar>
               <p>বার চার্ট</p>
-            </div>
+            </div> -->
             <div class="cols">
               <b-avatar  href="/visualization/barChart/horizontal" class="mx-4" src="~assets/D3BarChart.svg" size="8rem"></b-avatar>
               <p>হরিজন্টাল বার চার্ট</p>
