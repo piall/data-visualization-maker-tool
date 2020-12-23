a37e1d2a329d87eef70dfb584e9f6309b9e117b7
mdPial <root.pial@gmail.com>
2020-12-23 16:09:40 +0600

    added new api

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const cors = require("cors");
-const User = require("./models/User");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const User = require('./models/User');
+// const popup = require('popups');
 
 var mongoDB = "mongodb://localhost:27017/data_viz";
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true,})
@@ -22,13 +24,47 @@ app.post("/signup", async(req, res, next) => {
   })
   // console.log(newUser)
   await newUser.save(err => {
-    if (!err) console.log("success")
-    
+    if (err) {
+      return res.status(400).json({
+        title: 'error',
+        error: 'email in use'
+      })
+    }
+    return res.status(200).json({
+      title: 'signup success'
+    })
   })
   // var result = await newUser.save();
   // if(!result) console.log("error occured");
   // console.log(result);
 })
+
+app.post('/loginUser', (req, res, next) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (err) return res.status(500).json({
+      title: 'server error',
+      error: err
+    })
+    if (!user) {
+      return res.status(401).json({
+        title: 'user not found',
+        error: 'invalid credentials'
+      })
+    }
+    if (!bcrypt.compareSync(req.body.password, user.password)) {
+      return res.status(401).json({
+        tite: 'login failed',
+        error: 'invalid credentials'
+      })
+    }
+    let token = jwt.sign({ userId: user._id}, 'secretkey');
+    return res.status(200).json({
+      title: 'login sucess',
+      token: token
+    })
+  })
+})
+
 
 const port = process.env.PORT || 5000;
 
